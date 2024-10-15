adfe887d0b5bdc1f75a00a577beb76736793259f
unknown <patilnirav70@gmail.com>
2024-10-15 10:43:10 +0530

    test1 husky

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Test Component</h1>
+      <p>This is a test component.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TestComponent',
+    mounted() {
+      console.log('TestComponent mounted'); // Intentional linting issue (no-console)
+    },
+    data() {
+      return {
+        message: 'Hello World' // Intentional linting issue (trailing comma)
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: red;
+  }
+  </style>
+  
